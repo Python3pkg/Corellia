d65b93036a92bf0d260db87a422b4d7999d13857
Teƒx <zhaomeng.zhu@gmail.com>
2012-12-20 20:29:15 +0800

    add setup

--- test/test_broker.py
+++
@@ -1,7 +0,0 @@
-import sys  
-sys.path.append('..')
-from Corellia import Broker
-
-if __name__ == '__main__':
-	b = Broker(8899, 8898)
-	b.run()
--- test/test_client.py
+++
@@ -1,9 +0,0 @@
-import sys  
-sys.path.append('..')
-from Corellia import Client, call
-
-if __name__ == '__main__':
-	c = Client(("localhost", 8899))
-	print c.add(6, 8)
-
-	print call(("localhost", 8899), "add", (6, 8))
